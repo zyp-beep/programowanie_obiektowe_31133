e0052af32c959686b36dbf316d14dbf359ad467b
Piotr Żyła <pzyla@edu.cdv.pl>
2025-10-11 10:57:33 +0200

    car example

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Security.AccessControl;
 
-Console.WriteLine("Hello, World!");
+Car car1 = new Car("VW", 2025);
+Car car2 = new Car("Audi", 2000);
+Car car3 = new Car("RollsRoyce", 2005);
+
+car1.ShowMe();
+car2.ShowMe();
+car3.ShowMe();
+
+Console.WriteLine("Your car: {0}", car1.model);
+class Car
+{
+    string model;
+    int year;
+
+    public string Model
+    {
+        get {return model;}
+    }
+
+    public void ShowMe()
+    {
+        Console.WriteLine("Model: {0}, Year: {1}", model, year);
+    }
+}
