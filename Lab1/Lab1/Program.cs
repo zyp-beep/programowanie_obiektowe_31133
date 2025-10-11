bdc0e32da2b344e2e957602d7668f24d474c0818
Piotr Żyła <pzyla@edu.cdv.pl>
2025-10-11 10:43:41 +0200

    Lab2

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-Console.WriteLine("Hello, World!");
+
+
+
+
+// Ex. 3
+/*
+int count = 0;
+while (count < 5)
+{
+    Console.WriteLine($"Iteracja numer {count+1}");
+    count++;
+}
+*/
+/*
+string password;
+do
+{
+    Console.WriteLine("Podaj hasło: ");
+    password = Console.ReadLine();
+    
+} while (password != "admin123");
+Console.WriteLine("Zalogowano pomyślnie!");
+*/
+/*
+int num = 0;
+string inpNum;
+do
+{
+    Console.WriteLine("Podaj liczbe: ");
+    inpNum = Console.ReadLine();
+    num = Convert.ToInt32(inpNum);
+} while (num < 1);
+    Console.WriteLine("jupi!!!");
+*/
+
+
+/*
+// Ex. 2
+
+string[] names = { "Adam", "Kacper", "Maja", "Alicja", "Aleks" };
+
+Console.WriteLine("Names:");
+
+*/
+
+
+
+/*
+// Ex. 1 
+
+const int reqAge = 18;
+const string accDeniedMsg = "You must be 18 years old.";
+const string accAllowedMsg = "Wellcome to my shop.";
+
+int age = 16;
+while (age < reqAge)
+{
+    
+}
+
+if (age < reqAge)
+{
+    Console.WriteLine(accDeniedMsg);
+}
+else
+{
+    Console.WriteLine(accAllowedMsg);
+}
+Console.ReadLine("Exit...");
+*/
